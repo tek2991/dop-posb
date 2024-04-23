5249b39435354b6fd4ec2c0a9b41a09370b146c8
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2024-04-23 17:40:14 +0530

    Added account openings migration

---
+++ database/migrations/2024_04_23_120415_count_of_account_openings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('count_of_account_openings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('office_id')->constrained();
+            $table->integer('sb')->default(0);
+            $table->integer('rd')->default(0);
+            $table->integer('mis')->default(0);
+            $table->integer('ppf')->default(0);
+            $table->integer('scss')->default(0);
+            $table->integer('ssa')->default(0);
+            $table->integer('td')->default(0);
+            $table->integer('mssc')->default(0);
+            $table->integer('nsc')->default(0);
+            $table->integer('kvp')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
